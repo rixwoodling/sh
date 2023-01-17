b617a5dc1b9bcb4d53f6d41c3da133cb797c59cb
Rix <rixwoodling@gmail.com>
2023-01-17 12:15:02 -0800

    Update find-by-hash2.sh

--- find-by-hash2.sh
+++ find-by-hash2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-find -H /lib/firmware/* ! -type d -exec md5sum {} + | sort -k 2 | sed 's/  /,/' | tee /usr/local/opt/file.txt
+find -H /lib/firmware/* ! -type d -exec md5sum {} + | sort -k 2 | sed 's/  /,/' # | tee /usr/local/opt/file.txt
 
