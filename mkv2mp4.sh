ada61722a722f601e4f4fddfc109faa132649924
Rix <rixwoodling@gmail.com>
2023-01-19 23:45:56 -0800

    Update mkv2mp4.sh

--- mkv2mp4.sh
+++ mkv2mp4.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # remux mkv to mp4, re-encode dts to aac
-for i in *.mkv; do ffmpeg -i $i -c:v copy -c:a aac -b:a 384k $i.mp4; done
+for i in *.mkv; do 
+    ffmpeg -i $i -c:v copy -c:a aac -b:a 384k $i.mp4; 
+done
 
